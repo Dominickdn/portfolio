8bb8b52d7e3ae5d2bb90d98a09d4fac5fca4920e
Dominickdn <116680944+Dominickdn@users.noreply.github.com>
2024-10-14 20:28:50 +0200

    better fix changed z-index

--- portfolio-project/src/components/Links.jsx
+++ portfolio-project/src/components/Links.jsx
@@ -13,7 +13,7 @@ const Links = () => {
  
 return (
         <>
-            <section className="flex flex-col fixed right-0 top-0 pr-2 custom-font">
+            <section className="flex flex-col fixed right-0 top-0 pr-2 custom-font z-20">
     
                     <div className="flex items-center justify-center p-5">
                         <a href="https://www.linkedin.com/in/dominick-das-neves-503a4b23a/" target="_blank">
@@ -25,7 +25,7 @@ return (
                             <Github width={38} height={38}/>
                         </a>
                     </div>
-                    <div className="flex items-center justify-center p-5">
+                    <div className="flex items-center justify-center p-5 ">
                         <a href="/Resume-Dominick-Das-Neves (updated).pdf" target="_blank">
                             <FileDown width={38} height={38}/>
                         </a>
