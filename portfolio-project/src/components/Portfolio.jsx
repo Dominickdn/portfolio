dcc0d5ed668fcaca2dc91a5f43c2863213a746f6
Dominickdn <116680944+Dominickdn@users.noreply.github.com>
2025-02-24 20:08:26 +0200

    Updated projects

--- portfolio-project/src/components/Portfolio.jsx
+++ portfolio-project/src/components/Portfolio.jsx
@@ -10,110 +10,82 @@ function Portfolio() {
 
           <div className="flex justify-center lg:w-3/5 md:w-9/12 sm:w-8/12 p-4">
             <div className="flex flex-col border-pink-200 border-2 rounded-2xl w-full bg-stone-900"> 
-              <div className="flex p-10 font-semibold text-lg">
-                <p>Online storefront</p>
-              </div>
-              <div className="flex flex-col lg:flex-row"> 
-                <div className="flex justify-center items-start font-medium text-lg w-full lg:w-1/2 lg:pt-10 md: sm:2">
-                  <img className="border-pink-200 border-2 rounded-2xl  w-80 h-80 object-cover" src="/sputnik-des.jpg" alt="Sputnik Design" /> 
-                </div>
-                <div className="flex-1 flex flex-col justify-center p-10 text-md">
-                  <p>Key Features:
-                    <br />
-                    - Next.js: Utilized for fast performance and SEO-friendly pages.
-                    <br />
-                    - Shopify Integration: Manages products, orders, and payments through Shopify’s API.
-                    <br />
-                    - Responsive Design: Optimized for all devices with a mobile-first approach.
-                    <br />
-                    - User-Friendly: Easy navigation, detailed product pages, and a smooth checkout process.
-                  </p>
-                </div>
 
-              </div>
-              <div className="flex-1 flex flex-col justify-center px-10 text-md pb-10">
-                <br/>
-                  <p>
-                    Development Highlights:
-                    <br />
-                    - Server-Side Rendering: Ensures quick loading times and better SEO.
-                    <br />
-                    - Custom Components: Tailored features for a seamless shopping experience.
-                    <br />
-                  </p>
-                </div>
-                <div className="flex-1 flex flex-col justify-center px-10 text-md pb-10">
-                  <p>
-                    Overview:
-                    <br/>
-                    Sputnik Wonderwash is an online store built with Next.js and integrated with the Shopify API. 
-                    It provides an efficient and engaging shopping experience for premium cleaning products.
-                    <br />
-                    <a href="https://sputnikwashingmachine.co.za" target="_blank">Project hosted here(Desktop & Mobile)</a>
-                  </p>
-                </div>
-
-                <div className="flex pt-10 p-5 font-semibold text-lg">
-                  <p>Learning Exercises
+                <div className="flex pt-10 px-5 font-semibold text-lg">
+                  <p>Projects
                   <br/>
-                    1. Three.js Scene with Pointer Lock Controllers
+                    1. Sputnik Storefront
                   </p>
                 </div>
 
               <div className="flex flex-col lg:flex-row w-full"> 
-              <div className="flex justify-center items-start font-medium text-md w-full lg:w-1/2 lg:pt-10 md: sm:2 pb-10">
-                  <img className="w-80 h-80 object-cover border-pink-200 border-2 rounded-2xl " src="/pointer.jpg" alt="Pointer Lock controller" />
+              <div className="flex justify-center items-start font-medium text-md w-full lg:w-1/2 lg:pt-10 md: sm:2 pb-8">
+                  <img className="w-80 h-80 object-cover border-pink-200 border-2 rounded-2xl " src="/sputnik-des.jpg" alt="Pointer Lock controller" />
                 </div>
                 <div className="flex-1 flex flex-col justify-center px-10 text-md pb-10 pt-2">
                   <p>
-                  - Description: 
-                  created an interactive 3D scene using Three.js, featuring pointer lock controllers for an immersive scene.
+                  <br/>
+                  A fully-customized e-commerce website using Next.js, integrated with Shopify as a backend. The project focused on building a fast, seamless shopping experience with optimized performance and responsive design for both desktop and mobile users.
                     <br/>
-                    <br/>
-                  - Key Learnings:
-                    Mastered Three.js for 3D graphics and scene creation.
-                    Implemented pointer lock controllers for enhanced user interaction.
-                    Applied texture mapping.
                     
-                    <br />
-                    <a href="https://sage-sprinkles-38c9cc.netlify.app" target="_blank">Project hosted here(Desktop Recommended)</a>
-                  </p>
-                  
+                    <br/>
+                    Next.js, React, Tailwind, Shopify, Vercel
+                    <br/>
+                    <a href="https://www.sputnikwashingmachine.co.za" target="_blank">Project hosted here(Desktop Recommended)</a>
+                  </p>  
                 </div>
-                
-                
-                
               </div>
-              <div className="flex pt-10 p-5 font-semibold text-lg">
+
+              <div className="flex px-5 font-semibold text-lg">
+                  <p>
+                    2. NISI Storefront
+                  </p>
+                </div>
+
+              <div className="flex flex-col lg:flex-row w-full"> 
+              <div className="flex justify-center items-start font-medium text-md w-full lg:w-1/2 lg:pt-10 md: sm:2 pb-8">
+                  <img className="w-80 h-80 object-cover border-pink-200 border-2 rounded-2xl " src="/NISI.jpg" alt="Nisi" />
+                </div>
+                <div className="flex-1 flex flex-col justify-center px-10 text-md pb-10 pt-2">
                   <p>
                   <br/>
-                  2. 2D Plane Game made with Vanilla JavaScript.
-                  </p>
-                </div>
-              <div className="flex flex-col lg:flex-row"> 
-              
-              <div className="flex justify-center items-start font-medium text-md w-full lg:w-1/2 lg:pt-10 md: sm:2 pb-10">
-                  <img className="w-80 h-80 object-cover border-pink-200 border-2 rounded-2xl " src="/plane.jpg" alt="Pointer Lock controller" /> 
-              </div>
-              
-              <div className="flex-1 flex flex-col justify-center px-10 text-md pb-10 pt-2">
-                  <p>
-                - Description: A simple 2D game created to understand JavaScript fundamentals and game mechanics.
-                  Key Learnings: JavaScript basics, game logic, and programming fundamentals.
-
+                  A high-performance e-commerce website for NISI using Next.js, integrated with Shopify as the backend. Focused on SEO and optimization, ensuring fast load times and improved search engine visibility while providing a seamless, responsive experience across all devices.
                     <br/>
-                    <br/>
-                  - Key Learnings: JavaScript basics, game logic, and programming fundamentals.
-
                     
                     <br />
-                    <a href="https://plane-game-qyq4hm9hr-dominicks-projects-2913cbf2.vercel.app" target="_blank">Project hosted here(Desktop Recommended)</a>
+                    
+                    Next.js, React, Tailwind CSS, Shopify, Vercel, Figma
+                    <br/>
+                    <a href="https://commerce-main-sooty.vercel.app" target="_blank">Project hosted here(Desktop Recommended)</a>
+                  </p>  
+                </div>
+              </div>
+
+              <div className="flex px-5 font-semibold text-lg">
+                  <p>
+                    2. MDX editor
                   </p>
-                  
+              </div>
+
+              <div className="flex flex-col lg:flex-row w-full"> 
+              <div className="flex justify-center items-start font-medium text-md w-full lg:w-1/2 lg:pt-10 md: sm:2 pb-8">
+                  <img className="w-80 h-80 object-cover border-pink-200 border-2 rounded-2xl " src="/mdx.jpg" alt="MDX editor" />
                 </div>
+                <div className="flex-1 flex flex-col justify-center px-10 text-md pb-10 pt-2">
+                  <p>
+                  <br/>
+                  An interactive MDX editor made in Next.js that allows users to create, edit, and delete blog posts. The app supports embedding React components and custom elements within content, and features both light and dark mode. Posts are dynamically updated and saved to a Supabase database.
+                    <br/>
+                    
+                    <br />
+                    <br/>
+                    Next.js, MDX, React, Supabase
+                    <br/>
+                    <a href="https://blogposteditor.vercel.app" target="_blank">Project hosted here(Desktop Recommended)</a>
+                  </p>  
                 </div>
+              </div>
               
-                
             </div>
           </div>
         </section>
